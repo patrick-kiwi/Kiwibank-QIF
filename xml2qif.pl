3b0280ef037209064b7fd7145f9a96ab7ff469a6
Patrick O'Connor <patrick_kiwi@hotmail.com>
2014-10-26 01:06:45 -0700

    Update xml2qif.pl

--- xml2qif.pl
+++ xml2qif.pl
@@ -1,66 +1,59 @@
 #!/usr/bin/perl
 
-# Takes a bunch of Kiwibank XML files and converts them into
-# QIF files - one QIF file per bank account number!
-# Good for scraping bank records into GNUcash accountancy software
+# Takes multiple Kiwibank XML files and merges them them into
+# QIF files. The QIF files are divided into bank account numbers!
+# Good for for scraping bank records into GNUcash accountancy software
 # Author: Patrick O'Connor (patrick_kiwi@protonmail.ch)
 
 use XML::Simple qw(:strict);
 use Finance::QIF;
 use Data::Dumper;
 
-# Specify location of .xml files,
-# (one directory up from perl executable)
-
-my $xmldir = "./xmlfiles/";
+my $xmldir = "./xmlfiles/"; #Specify XML directory location
 opendir(DIR, "$xmldir");
 my @FILES= readdir(DIR);
 closedir(DIR);
 
-#global variables
-my $account_numbers; #array ref containing all account numbers
+foreach $file (@FILES) { 
+if ($file ne "." && $file ne "..") {#file loop
+#print "Loading $file\n";
 
-foreach $file (@FILES) { #File Loop
-if ($file ne "." && $file ne "..") {#If real files loop
-print "Processing $file\n";
-
- 
 my $config = XMLin("$xmldir$file", 
 KeyAttr => { Account => 'MOD11Number' },
 ForceArray => ['Account', 'Transaction', 'Line'],
 SuppressEmpty => 1,
-);
+); #load XML transactions records into a data container
 
-#print Dumper($config);
+#print Dumper($config); #For debugging - show data container structure
 
-foreach ( keys %{$config->{'Account'}}) { #AccountNumberLoop
-push @$account_numbers, $_;		  #load array ref again
-}
-
-foreach my $account_number (@$account_numbers) {
-#print "$account_number\n";
-my $qif = Finance::QIF->new( file => "+>>${account_number}.qif" );
+foreach my $account_number (keys %{$config->{'Account'}}) { #AccountNumberLoop
+	my $OUTQIF = Finance::QIF->new( file => ">>${account_number}.qif" ); #define QIF object
+	#print "Processing transactions from $account_number\n";
 	foreach $tran_ref 
 	( @{$config->{'Account'}->{"$account_number"}->{'Transaction'}} ) 
-	{ #Transaction (per account number) loop
+	{ #Transactions (belongning to account number) loop
 	my $amount = $tran_ref->{'Lines'}->{'Line'}->[0]->{'Amount'};
 	my $date = $tran_ref->{'Date'};
 	$date =~ s/(\d+)\-(\d+)\-(\d+)/$3\/$2\/$1/;
 	my $payee = $tran_ref->{'Lines'}->{'Line'}->[0]->{'Description'};
 	my $memo = $tran_ref->{'Lines'}->{'Line'}->[1]->{'Description'};
 
-		my $record = {
+		my $record = { #load each transaction into a hash ref
     		header   	=> "Type:Bank", 
     		transaction   	=> "$amount",
     		payee    	=> "$payee",
     		memo     	=> "$memo",
     		date     	=> "$date",
   		};
-	$qif->header( $record->{header} );
-  	$qif->write($record);
-	}##Transaction (per account number) loop
-$qif->close;
-}##Account number loop
-}###If real file loop
-}##File loop	
+	
+	$OUTQIF->header( $record->{header} );
+  	$OUTQIF->write($record);
+	%$record = (); #empty the hash ref
+
+	}#Close Transactions (belongning to account number) loop
+	
+	$OUTQIF->close();
+}##Close Account number loop
+}##Close File loop
+}
 	
